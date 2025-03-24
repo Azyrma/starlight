15c1935e548dba8ad0545047bd504873ca5fcba9
Azyrma <severin.mills@gmail.com>
2025-03-25 00:36:55 +0100

    Initial commit.

---
+++ astro.config.mjs
@@ -0,0 +1,39 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import starlightThemeRapide from 'starlight-theme-rapide';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			plugins: [starlightThemeRapide()],
+			title: 'Severin Mills',
+			social: {
+				github: 'https://github.com/Azyrma',
+				linkedin: 'https://www.linkedin.com/in/severin-mills-4b9609220/',
+				email: 'mailto:smills@ethz.ch'
+			},
+			sidebar: [
+				{
+					label: 'Home',
+					items: [
+						{ label: 'About me', slug: 'home'},
+					],
+				},
+				{
+					label: 'Information Retrieval',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'General Information', slug: 'infret/general' },
+						{ label: 'Exercise session', slug: 'infret/exercise'}
+					],
+				},
+				// {
+				// 	label: 'Einführung in die Programmierung',
+				// 	autogenerate: { directory: 'reference' },
+				// },
+			],
+		}),
+	],
+});
---
+++ src/content/docs/home.md
@@ -0,0 +1,8 @@
+---
+title: Welcome to my homepage.
+description: Info about me.
+---
+
+ I am a fourth semester BSc student and I am currently a teaching assistant in Information Retrieval and also a Board Member in VIS. Apart from ETH, I like to go skiing, trail-running and play video games. Don’t ask me how I have time for all of this (I don’t).
+
+ You can use the sidebar to navigate and find the relevant information you need.
