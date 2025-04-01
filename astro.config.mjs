901bf6da0f5dd96f49b0969b688c4bda9ed2b2c9
Azyrma <severin.mills@gmail.com>
2025-04-01 12:57:06 +0200

    Changed font.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import starlightThemeRapide from 'starlight-theme-rapide';
+import '@fontsource-variable/jetbrains-mono';
 
 import vercel from '@astrojs/vercel';
 
@@ -10,39 +11,41 @@ export default defineConfig({
   output: 'server',
 
   integrations: [
-      starlight({
-          plugins: [starlightThemeRapide()],
-          title: 'Severin Mills',
-          social: {
-              github: 'https://github.com/Azyrma',
-              linkedin: 'https://www.linkedin.com/in/severin-mills-4b9609220/',
-              email: 'mailto:smills@ethz.ch'
-          },
-          sidebar: [
-              {
-                  label: 'Home',
-                  items: [
-                      { label: 'About me', slug: 'home'},
-                  ],
-              },
-              {
-                  label: 'Information Retrieval',
-                  items: [
-                      // Each item here is one entry in the navigation menu.
-                      { label: 'General Information', slug: 'infret/general' },
-                      { label: 'Exercise session', slug: 'infret/exercise'}
-                  ],
-              },
-              // {
-              // 	label: 'Einführung in die Programmierung',
-              // 	autogenerate: { directory: 'reference' },
-              // },
-          ],
-      }),
-	],
+    starlight({
+        
+        plugins: [starlightThemeRapide()],
+        customCss: ['./src/styles/custom.css'],
+        title: 'Severin Mills',
+         social: {
+            github: 'https://github.com/Azyrma',
+            linkedin: 'https://www.linkedin.com/in/severin-mills-4b9609220/',
+            email: 'mailto:smills@ethz.ch'
+        },
+        sidebar: [
+            {
+                label: 'Home',
+                items: [
+                    { label: 'About me', slug: 'home'},
+                ],
+            },
+            {
+                label: 'Information Retrieval',
+                items: [
+                    // Each item here is one entry in the navigation menu.
+                    { label: 'General Information', slug: 'infret/general' },
+                    { label: 'Exercise session', slug: 'infret/exercise'}
+                ],
+            },
+            // {
+            // 	label: 'Einführung in die Programmierung',
+            // 	autogenerate: { directory: 'reference' },
+            // },
+        ],
+    }),
+],
     adapter: vercel({
         webAnalytics: {
             enabled: true,
         },
-      }),
+    }),       
 });
